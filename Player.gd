d40adf703a7aa8c82a3b1cff6d71ffa72b97588a
Rafael F Pessoa <112719981+RafaFonPessoa@users.noreply.github.com>
2024-07-01 16:01:33 -0300

    Teste de Movimentação

---
+++ Player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+var speed = 300
+var accel = 10
+
+var can_dodge = true
+
+func _physics_process(delta):
+	player_moviment()
+	move_and_slide()
+
+func player_moviment():
+	var direction = Input.get_vector("left", "right", "up", "down").normalized()
+	
+	if direction:
+		velocity.x = move_toward(velocity.x ,direction.x * speed, accel)
+		velocity.y = move_toward(velocity.y, direction.y * speed, accel)
+	else:
+		velocity.x = move_toward(velocity.x, 0, accel)
+		velocity.y = move_toward(velocity.y, 0, accel)
+	
+	if Input.is_action_pressed("dodge") and can_dodge:
+		velocity.x = speed * 2 * direction.x
+		velocity.y = speed * 2 * direction.y
+		can_dodge = false
+		$DodgeTimer.start(1)
+
+func _on_dodge_timer_timeout():
+	can_dodge = true
+	velocity.x = 0
+	velocity.y = 0
+	$DodgeTimer.stop()
+
