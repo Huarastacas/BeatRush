5a1d8ec1b7da66b6e7e7327f67ef6e72bc27601a
Huarastaca <huarastaca@gmail.com>
2024-07-05 21:31:39 -0300

    balas lançadas

--- Player.gd
+++ Player.gd
@@ -1,15 +1,32 @@
 extends CharacterBody2D
 
+#varaiveis do player
 var speed = 300
 var accel = 10
-
 var can_dodge = true
+
+#var spawn da bala
+var bullet_speed = 1000
+var bullet = preload("res://bullet_test.tscn")
+
+
+func fire():
+	var bullet_instance = bullet.instantiate()
+	bullet_instance.position = get_global_position()
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.linear_velocity = Vector2(bullet_speed,0).rotated(rotation)
+	#bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed,0).rotated(rotation))
+	get_tree().get_root().call_deferred("add_child",bullet_instance)
+
 
 func _physics_process(delta):
 	player_moviment()
 	move_and_slide()
 
 func player_moviment():
+	
+	look_at(get_global_mouse_position())
+	
 	var direction = Input.get_vector("left", "right", "up", "down").normalized()
 	
 	if direction:
@@ -24,10 +41,16 @@ func player_moviment():
 		velocity.y = speed * 2 * direction.y
 		can_dodge = false
 		$DodgeTimer.start(1)
+		
+	if Input.is_action_just_pressed("shoot"):
+		fire()
 
 func _on_dodge_timer_timeout():
 	can_dodge = true
 	velocity.x = 0
 	velocity.y = 0
 	$DodgeTimer.stop()
+	
+	
+
 
